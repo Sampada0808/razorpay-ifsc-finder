333e196932460ae70c374d293909b3d2f6fb0355
Sampada0808 <sampadashankar88@gmail.com>
2025-06-16 12:44:44 +0530

    Initial Commit

---
+++ ifsc-lookup-app/ifsc_lookup_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ifsc_lookup_appApp.swift
+//  ifsc-lookup-app
+//
+//  Created by Sampada Shankar on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ifsc_lookup_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
